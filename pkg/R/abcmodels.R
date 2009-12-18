c8ffffa6a01188642287d8e6d8cd8198f989e7b9
bcomeara <bcomeara@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-12-18 22:30:53 +0000

    temporary upload of file with batch commands

--- pkg/R/abcmodels.R
+++ pkg/R/abcmodels.R
@@ -1,16 +1,37 @@
 #models
+#note that these work for univariate, but need to be generalized for multivariate
+#otherstates has one row per taxon, one column per state
+#states is a vector for each taxon, with length=nchar
+
+nullExtrinsic<-function(params,selfstates,otherstates) {
+	newstates<-0*selfstates
+	return(newstates)
+}
+
+nullIntrinsic<-function(params,states) {
+	newstates<-0*states
+	return(newstates)
+}
+
+
 
 brownianIntrinsic<-function(params,states) {
 	newstates<-rnorm(n=length(states),mean=states,sd=params)
 	return(newstates)
 }
 
-brownianExtrinsic<-function(params,selfstates,otherstates) {
-	newstates<-0*selfstates
+thresholdIntrinsic<-function(params, states) {
+	#params[2] is min, params[3] is max. params[2] could be 0 or -Inf, for example
+	newstates<-min(max(rnorm(n=length(states),mean=states,sd=params[1]),params[2]),params[3])
+	for (i in length(newstates)) {
+		newstates[i]<-max(newstates[i],params[2]) #whichever is larger, the current value or the min value
+		newstates[i]<-min(newstates[i],params[3])
+	}
 	return(newstates)
+
 }
 
-autoregressiveIntrinsic<-function(params,states) { #a discrete time OU
+autoregressiveIntrinsic<-function(params,states) { #a discrete time OU, same sd, mean, and attraction for all chars
 	sd<-params[1]
 	attractor<-params[2]
 	attraction<-params[3]	#in this model, this should be between zero and one
@@ -18,13 +39,11 @@ autoregressiveIntrinsic<-function(params,states) { #a discrete time OU
 	return(newstates)
 }
 
-nullExtrinsic-function(params,selfstates,otherstates) {
-	newstates<-0*selfstates
+nearestNeighborDisplacementExtrinsic<-function(params,selfstates,otherstates) { #this is set up for one character only right now
+	repulsor<-otherstates[which.min(abs(otherstates-selfstates)),1] #the ,1 makes it for one char only
+	#add things here
+	
 	return(newstates)
 }
 
-nullIntrinsic-function(params,states) {
-	newstates<-0*states
-	return(newstates)
-}
 
