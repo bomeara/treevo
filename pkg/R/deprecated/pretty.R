56bda2f99a384de46d76da7c88a5c3989bb9d4c3
bcomeara <bcomeara@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-01-14 22:05:05 +0000

    no need for this code in TreEvo

--- pkg/R/deprecated/pretty.R
+++
@@ -1,5 +0,0 @@
-#creates a plot good for figures in talks, etc.
-prettyTree<-function(ntax=10,edge.width=10)
-{
-    plot(ladderize(chronogram(rtree(ntax)),right=FALSE),show.tip.label=FALSE,direction="upwards",edge.width=edge.width)
-}
