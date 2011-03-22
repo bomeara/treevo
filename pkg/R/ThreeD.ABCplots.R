f8e482349f01d9078b1b7ce6c426de116ebbabec
bbanbury <bbanbury@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-03-22 18:42:38 +0000

    added NewPlotPriorsPosteriors.R

--- pkg/R/ThreeD.ABCplots.R
+++ pkg/R/ThreeD.ABCplots.R
@@ -14,7 +14,7 @@ param.position<-parameter
 	r<-vector() #vector of y vals
 	s<-vector() #generation each x-y coord is found
 
-	if (range(subset(a[which(a$weight>0),])[,param.position])!=0) {
+	if (max(subset(a[which(a$weight>0),])[,param.position])-min(subset(a[which(a$weight>0),])[,param.position])!=0) {
 	v<-vector("list", max(a$generation))
 		for (i in 1:max(a$generation)){
 			which.gen<-(i+1)-1
@@ -36,7 +36,7 @@ param.position<-parameter
 
 		plot3d(x, y, z, col="black", box=FALSE, type="n", xlab="", ylab="", zlab="", zlim=c(0, max(a$generation)), ylim=c(0, max(y)))
 		rgl.viewpoint(35, 1, 90)  #sets viewpoint for initial plot
-		title3d(colnames(a)[param.position], col='red', pos=c(NA, 0, max(z))) 
+		title3d(colnames(a)[param.position], col='red', pos=c(NA, -2, max(z))) 
 		#text3d(x=min(x), y=mean(y), z=max(z), text="Density" col='blue') 
 		#title3d("Starting States", col='purple', pos=c(NA, 0, max(which.gen))) 
 
@@ -46,6 +46,8 @@ param.position<-parameter
 			zfit<-predict(lm(z[which(z==i)] ~ x[which(z==i)] + y[which(z==i)]), newdata=data.frame(x=triangles[,1], y=triangles[,2]))
 			opacity<-0.8*(ngen/length(v))
 			rgl.material(color="black", alpha=opacity, lit=FALSE)
+			#return(dim(triangles))
+			#return(dim(zfit))
 			triangles3d(cbind(triangles, zfit), col="red")
 			#readline(prompt="hit enter ")
 			if (realParam) {
@@ -69,12 +71,15 @@ short.kept<-subset(kept[which(kept$generation>1),])[,]
 		cat("currently not plotting particles.  To plot particles modify the show.particles= argument.")
 		}
 	else if (show.particles=="weights") {
-				#for (i in 1:nrow(reject)) {	
-		#	particles3d(x=reject[i, param.position], y=-1*(.05*(max(y)-min(y))), z=reject[i, 1], radius=.3, type="s", col="red", add=TRUE, aspect=TRUE)
+		#for (i in 1:nrow(reject)) {	
+		#	spheres3d(x=reject[i, param.position], y=-1*(.05*(max(y)-min(y))), z=reject[i, 1], radius=.3, type="s", col="gray", add=TRUE, aspect=TRUE)
+			#particles3d(x=reject[i, param.position], y=-1*(.05*(max(y)-min(y))), z=reject[i, 1], radius=.3, type="s", col="red", add=TRUE, aspect=TRUE)
 		#}
 		for (j in 1:nrow(kept)) {
-			circle.size<-(kept[j, 6]/max(kept[,6]))*(.1*(max(kept[,param.position])-min(kept[,param.position])))
-			particles3d(x=kept[j, param.position], y=-1*(.05*(max(y)-min(y))), z=kept[j, 1], radius=circle.size, type="s", col="black", add=TRUE, aspect=TRUE)
+			circle.size<-(kept[j, 6]/max(kept[,6]))*(.1*(max(kept[,param.position])-min(kept[,param.position])))*10
+			spheres3d(x=kept[j, param.position], y=-1*(.05*(max(y)-min(y))), z=kept[j, 1], radius=circle.size, type="s", col="black", add=TRUE, aspect=TRUE)
+			#rgl.points("points", x=kept[j, param.position], y=-1*(.05*(max(y)-min(y))), z=kept[j, 1], radius=circle.size, type="s", col="black", add=TRUE, aspect=TRUE)
+			#particles3d(x=kept[j, param.position], y=-1*(.05*(max(y)-min(y))), z=kept[j, 1], radius=circle.size, type="s", col="black", add=TRUE, aspect=TRUE)
 		}	
 	}		
 	else if (show.particles=="distance") {
@@ -82,8 +87,9 @@ short.kept<-subset(kept[which(kept$generation>1),])[,]
 		#	particles3d(x=reject[i, param.position], y=-1*(.05*(max(y)-min(y))), z=reject[i, 1], radius=.3, type="s", col="red", add=TRUE, aspect=TRUE)
 		#}
 		for (j in 1:nrow(kept)) {
-			circle.size<-(kept[j, 5]/max(kept[,5]))*(.1*(max(kept[,param.position])-min(kept[,param.position])))
-			particles3d(x=kept[j, param.position], y=-1*(.05*(max(y)-min(y))), z=kept[j, 1], radius=circle.size, type="s", col="black", add=TRUE, aspect=TRUE)
+			circle.size<-(kept[j, 5]/max(kept[,5]))*(.1*(max(kept[,param.position])-min(kept[,param.position])))*15
+			spheres3d(x=kept[j, param.position], y=-1*(.05*(max(y)-min(y))), z=kept[j, 1], radius=circle.size, type="s", col="black", add=TRUE, aspect=TRUE)
+			#particles3d(x=kept[j, param.position], y=-1*(.05*(max(y)-min(y))), z=kept[j, 1], radius=circle.size, type="s", col="black", add=TRUE, aspect=TRUE)
 		}	
 	}	
 	if (plot.parent){
