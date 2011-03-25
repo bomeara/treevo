5a8ca8e519326e9984327853f5c39c8fabc2709c
Barb Banbury <bbanbury@users.noreply.github.com>
2011-03-25 20:35:33 +0000

    added ExponentiallyDecayingPush to abcmodels.R

--- R/abcmodels.R
+++ R/abcmodels.R
@@ -65,10 +65,27 @@ nearestNeighborDisplacementExtrinsic<-function(params,selfstates,otherstates, ti
 	springK<-params[2]
 	maxforce<-params[3]
 	localsign<-sign(selfstates[1]- repulsorValue)
+	#print(abs((selfstates[1]-repulsorValue)))
 	if(localsign==0) { 
 		localsign=sign(rnorm(n=1))	
 	}
 	newdisplacement<-rnorm(n=1,mean=localsign*min(c(abs(springK/((selfstates[1]-repulsorValue)*(selfstates[1]-repulsorValue))),maxforce),na.rm=TRUE),sd=sd)
+	return(newdisplacement)
+}
+
+ExponentiallyDecayingPush<-function(params,selfstates,otherstates, timefrompresent) { 
+	#params[1] is sd, params[2] is maxForce when character difference = 0, params[3] is half distance (the phenotypic distance at which repulsion is half maxForce)
+	repulsorTaxon<-which.min(abs(otherstates-selfstates))
+	repulsorValue<-otherstates[repulsorTaxon]
+	sd<-params[1]
+	maxForce<-params[2]
+	halfDistance<-params[3] #is like half life
+	rate<-log(2, base = exp(1))/ halfDistance
+	localsign<-sign(selfstates[1]- repulsorValue)
+	if(localsign==0) {  #to deal with case of identical values
+		localsign=sign(rnorm(n=1))	
+	}
+	newdisplacement<-rnorm(n=1,mean=maxForce*localsign*exp(-1*rate*abs((selfstates[1]-repulsorValue))),sd=sd)
 	return(newdisplacement)
 }
 
