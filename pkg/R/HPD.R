9d959b61a152b914917da6d47aa57945424868e2
bbanbury <bbanbury@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-03-28 03:07:59 +0000

    changed HPD function to use codas HPDinterval function instead of my sloppy one

--- pkg/R/HPD.R
+++ pkg/R/HPD.R
@@ -1,48 +1,24 @@
 HPD<-function(particleDataFrame, percent=0.95, returnData=F){
 	summary<-vector("list")
 	Ints<-matrix(nrow=(dim(particleDataFrame)[2]-6), ncol=4)
-	colnames(Ints)<-c("mean", "sd", "LowerHPD", "UpperHPD")	
+	colnames(Ints)<-c("mean", "sd", paste("LowerHPD_", percent, sep=""), paste("UpperHPD_", percent, sep=""))	
 	rownames(Ints)<-names(particleDataFrame[7: dim(particleDataFrame)[2]])
 	subpDF<-subset(particleDataFrame[which(particleDataFrame$weight>0),], generation==max(particleDataFrame$generation))[7:dim(particleDataFrame)[2]] 
-	if(dim(subpDF)[1] > 50){
-		for(i in 1:dim(subpDF)[2]){
-			if(sd(subpDF[,i]) != 0) {
-				a<-c()
-				b<-c()
-				lower<-c()
-				upper<-c()
-				interv<-c()
-				sortedValues<-sort(subpDF[,i])
-				numberInTails<-floor(length(sortedValues)*(1-percent))
-				for(j in 1:numberInTails){
-					a<-append(a, j)
-					b<-append(b, length(sortedValues)-j)
-					lower<-append(lower, sortedValues[j])
-					upper<-append(upper, sortedValues[length(sortedValues)-(numberInTails-j)])
-					interv<-append(interv, sortedValues[length(sortedValues)-(numberInTails-j)]-sortedValues[j]) 
-				}
-				intervalTable<-cbind(a, b, lower, upper, interv)
-				#print(intervalTable)
-				Ints[i,1]<-mean(subpDF[,i]) #not weighted
-				Ints[i,2]<-sd(subpDF[,i])
-				Ints[i,3]<-as.numeric(intervalTable[which(intervalTable[,5]==min(intervalTable[,5])),3])
-				Ints[i,4]<-as.numeric(intervalTable[which(intervalTable[,5]==min(intervalTable[,5])),4])
-				if (returnData){
-					low<-as.numeric(intervalTable[which(intervalTable[,5]==min(intervalTable[,5])),1])+1 #add one because you want the next value over
-					high<-as.numeric(intervalTable[which(intervalTable[,5]==min(intervalTable[,5])),2])-1
-					summary$returnedData<-sortedValues[low:high]
-					#print(summary)
-					if(sd(subpDF[,i]) != 0){
-						summary$returnedData<-NA
-					}
-				}		
-			}
+	for(i in 1:dim(subpDF)[2]){
+		if(sd(subpDF[,i]) != 0) {
+
+			Ints[i,1]<-mean(subpDF[,i]) #not weighted
+			Ints[i,2]<-sd(subpDF[,i])
+			Ints[i,3]<-HPDinterval(as.mcmc(subpDF[i]), prob=percent)[1] #returns lower HPD		
+			Ints[i,4]<-HPDinterval(as.mcmc(subpDF[i]), prob=percent)[2] #returns upper HPD	
+			if(returnData){
+				summary[[i]]<-subpDF[,i][-c(which(subpDF[,i]<Ints[i,3]), which(subpDF[,i]>Ints[i,4]))]		
+			}		
 		}
-		if(returnData){
-			summary$summary<-Ints
-			return(summary)
-		}
-		else{return(Ints)}
 	}
-	else(print(paste("Warning, there are only ", dim(subpDF)[1], "accepted particles, which is too few to calculate HPD")))
+	if(returnData){
+		summary$summary<-Ints
+		return(summary)
+	}
+	else{return(Ints)}
 }
