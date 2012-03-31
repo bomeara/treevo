b9f496166e6b3d7146771a1e005d03dee640d0af
Barb Banbury <bbanbury@users.noreply.github.com>
2012-03-31 20:36:10 +0000

    minor change to BCP

--- R/BCP.R
+++ R/BCP.R
@@ -1,18 +1,18 @@
-BCP<-function(RealParam, HPD, verbose=F){  #alter so it can be a single or a list of realParmas
 #BCP = Bayesian Coverage Probability
 #RealParam can be "RealParams$vector" from doSimulation c(x1, x2, ...) or a list
 #HPD should be a list of HPD output from different runs
 #Calculates what percent of the time the real parameter falls into the HPD
-if(class(RealParam)=="numeric"){
-	rps<-vector("list", length=length(HPD))
-	for (i in 1: length(HPD)){
-		rps[[i]]<-RealParam
+BCP<-function(RealParam, HPD, verbose=F){  #alter so it can be a single or a list of realParmas
+	if(class(RealParam)=="numeric"){
+		rps<-vector("list", length=length(HPD))
+		for (i in 1: length(HPD)){
+			rps[[i]]<-RealParam
+		}
 	}
-}
-rps<-RealParam
-#if(length(RealParam) != dim(HPD[[1]])[1]){ warning("RealParams and HPD do not match")}  #need something like this, but it will have to be after changing the RealParam to take a list 
-Covered<-matrix(nrow=length(HPD), ncol=length(rps[[i]]))
-colnames(Covered)<-rownames(HPD[[1]])
+	rps<-RealParam
+	#if(length(RealParam) != dim(HPD[[1]])[1]){ warning("RealParams and HPD do not match")}  #need something like this, but it will have to be after changing the RealParam to take a list 
+	Covered<-matrix(nrow=length(HPD), ncol=length(rps[[i]]))
+	colnames(Covered)<-rownames(HPD[[1]])
 	for(i in 1:length(HPD)){
 		for(j in 1:length(rps[[i]])){
 			if(!is.na(HPD[[i]][j,4])) { #keep only parameters that vary (ie, not fixed)
