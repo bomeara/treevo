6cd0b731bd3409cb6320894b61ce06cb3c7caf7c
bbanbury <bbanbury@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-04-17 17:07:30 +0000

    changed HPD function to accept abc data and a few other minor things

--- pkg/R/HPD.R
+++ pkg/R/HPD.R
@@ -1,19 +1,28 @@
-HPD<-function(particleDataFrame, percent=0.95, returnData=F){
+HPD<-function(particleDataFrame, abc=F, percent=0.95, returnData=F){
 	library(coda, quietly=T)
 	summary<-vector("list")
-	Ints<-matrix(nrow=(dim(particleDataFrame)[2]-6), ncol=4)
-	colnames(Ints)<-c("mean", "sd", paste("LowerHPD_", percent, sep=""), paste("UpperHPD_", percent, sep=""))	
-	rownames(Ints)<-names(particleDataFrame[7: dim(particleDataFrame)[2]])
-	subpDF<-subset(particleDataFrame[which(particleDataFrame$weight>0),], generation==max(particleDataFrame$generation))[7:dim(particleDataFrame)[2]] 
+	if (abc){
+		Ints<-matrix(nrow=(dim(particleDataFrame)[2]), ncol=4)
+		colnames(Ints)<-c("mean", "sd", paste("LowerHPD_", percent, sep=""), paste("UpperHPD_", percent, sep=""))	
+		rownames(Ints)<-names(particleDataFrame)
+		subpDF<-as.data.frame(particleDataFrame) 
+	}
+	else{
+		Ints<-matrix(nrow=(dim(particleDataFrame)[2]-6), ncol=4)
+		colnames(Ints)<-c("mean", "sd", paste("LowerHPD_", percent, sep=""), paste("UpperHPD_", percent, sep=""))	
+		rownames(Ints)<-names(particleDataFrame[7: dim(particleDataFrame)[2]])
+		print(class(particleDataFrame))
+		subpDF<-as.data.frame(subset(particleDataFrame[which(particleDataFrame$weight>0),], generation==max(particleDataFrame$generation))[7:dim(particleDataFrame)[2]])
+	}
 	for(i in 1:dim(subpDF)[2]){
 		if(sd(subpDF[,i]) != 0) {
 
 			Ints[i,1]<-mean(subpDF[,i]) #not weighted
 			Ints[i,2]<-sd(subpDF[,i])
-			Ints[i,3]<-HPDinterval(as.mcmc(subpDF[i]), prob=percent)[1] #returns lower HPD		
-			Ints[i,4]<-HPDinterval(as.mcmc(subpDF[i]), prob=percent)[2] #returns upper HPD	
+			Ints[i,3]<-HPDinterval(as.mcmc(subpDF[,i]), prob=percent)[1] #returns lower HPD		
+			Ints[i,4]<-HPDinterval(as.mcmc(subpDF[,i]), prob=percent)[2] #returns upper HPD	
 			if(returnData){
-				summary[[i]]<-subpDF[,i][-c(which(subpDF[,i]<Ints[i,3]), which(subpDF[,i]>Ints[i,4]))]		
+				summary[[i]]<-subpDF[,i][-c(which(subpDF[i]<Ints[i,3]), which(subpDF[,i]>Ints[i,4]))]		
 			}		
 		}
 	}
