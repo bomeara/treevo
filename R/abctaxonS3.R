2b25143d85658b1f710d362a94d1290a17cba58b
Brian O'Meara <bomeara@users.noreply.github.com>
2012-06-16 04:27:24 +0000

    now have s3 working for simulation (cutting simulation time by 2/3). Not yet adapted for doRun or doRun_smc

--- R/abctaxonS3.R
+++ R/abctaxonS3.R
@@ -1,6 +1,6 @@
 #create abctaxonS3 class object
 abctaxonS3 <- function(id=0, name="taxon0", timeSinceSpeciation=0, states=NA, nextstates=NA) {
-	taxon <- list(id=id, name=name, timeSinceSpeciation=timeSinceSpeciation, state=states, 
+	taxon <- list(id=id, name=name, timeSinceSpeciation=timeSinceSpeciation, states=states, 
 		nextstates=nextstates)
 	class(taxon) <- "abctaxonS3"
 	return(taxon)
