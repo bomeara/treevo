925345041a15bebe511055442b4faa4d9f46335c
Barb Banbury <bbanbury@users.noreply.github.com>
2012-06-22 21:22:37 +0000

    altered doRun_rej for distances, object names, and output in particleDataFrame format

--- R/doRun_rej.R
+++ R/doRun_rej.R
@@ -113,39 +113,46 @@ doRun_rej<-function(phy, traits, intrinsicFn, extrinsicFn, summaryFns=c(rawValue
 	trueFreeValuesMatrix<-trueFreeValuesANDSummaryValues[,1:numberParametersFree]
 	summaryValuesMatrix<-trueFreeValuesANDSummaryValues[,-1:-numberParametersFree]
 	
+	#boxcox transform summary values
 	boxcoxEstimates<-boxcoxEstimation(summaryValuesMatrix)
 	boxcoxAddition<-boxcoxEstimates$boxcoxAddition
 	boxcoxLambda<-boxcoxEstimates$boxcoxLambda
 	boxcoxSummaryValuesMatrix<-boxcoxEstimates$boxcoxSummaryValuesMatrix
-	boxcoxOriginalSummaryStats<-boxcoxTransformation(summaryStatsLong(phy=phy, data=traits),boxcoxAddition, boxcoxLambda)
+	boxcoxOriginalSummaryStats<-boxcoxTransformation(summaryStatsLong(phy=phy, data=traits), boxcoxAddition, boxcoxLambda)
 
 	
-	GetVipSingleColumn<-function(trueFreeValuesColumn,boxcoxSummaryValuesMatrix) {
-		return(vip(pls(X=boxcoxSummaryValuesMatrix,Y=trueFreeValuesColumn,ncomp=1)))
+	getVipSingleColumn<-function(trueFreeValuesColumn, boxcoxSummaryValuesMatrix) {
+		return(vip(pls(X=boxcoxSummaryValuesMatrix, Y=trueFreeValuesColumn, ncomp=1)))
 	}
 	
 	#note this has vip for summary val 5, for true param 1, in row 5, column 1, and so forth
-	allVip<-apply(trueFreeValues,2,GetVipSingleColumn,boxcoxSummaryValuesMatrix)
+	allVip<-apply(trueFreeValuesMatrix, 2, getVipSingleColumn, boxcoxSummaryValuesMatrix)
 	
 	#this will have which summary vals have importance above the threshold
 	whichVip<-(allVip>vipthresh)
 	
-	abcDistancesRaw<-rep(0,dim(trueFreeValues)[1]) #will hold the distances for each particle
-	
+	abcDistancesRaw<-rep(0,dim(trueFreeValuesMatrix)[1]) #will hold the distances for each particle
+	abcDistancesRawTotal<-vector(length=dim(trueFreeValuesMatrix)[1])
 	#now we go true parameter by true parameter, using only the summary values with enough importance for each
 	#we get a distance for each particle from the observed particle for each true param
 	#then get a euclidean distance for all of these
 	#it's like getting delta X, then delta Y, and figuring out distance from the origin using
 	#sqrt((X-0)^2 + (Y-0)^2)
-	for (freeParamIndex in sequence(dim(trueFreeValues)[2])) {
-		abcDistancesRaw<-abcDistancesRaw+(abc(target=boxcoxOriginalSummaryStats[whichVip[,freeParamIndex]], param=trueFreeValuesMatrix[freeParamIndex], sumstat= boxcoxSummaryValuesMatrix[,whichVip[,freeParamIndex] ], tol=1, method=abcMethod)$dist)^2 #because we're doing euclidean distance, from observed, which has value 0, 0, 0, etc.
+	for (freeParamIndex in sequence(dim(trueFreeValuesMatrix)[2])) {
+		abcDistancesRaw<-abc(target=boxcoxOriginalSummaryStats[whichVip[,freeParamIndex]], param=trueFreeValuesMatrix[,freeParamIndex], sumstat= boxcoxSummaryValuesMatrix[,whichVip[,freeParamIndex] ], tol=1, method=abcMethod)$dist^2 #because we're doing euclidean distance, from observed, which has value 0, 0, 0, etc.
+		abcDistancesRawTotal<-abcDistancesRawTotal+abcDistancesRaw
 	}
-	abcDistances<-sqrt(abcDistancesRaw) #Euclid rules.
-	abcResults<-trueFreeValuesMatrix[which(abcDistance<=quantile(abcDistance ,prob=abcTolerance)), ] #here's where we diy abc
-	
-	print(abcResults)
-	input.data<-rbind(jobName, length(phy[[3]]), timeStep, StartSims, standardDevFactor, abcMethod, abcTolerance)
+	abcResults<-vector("list")
+	abcDistances<-sqrt(abcDistancesRawTotal) #Euclid rules.
+	abcResults$unadj.values<-trueFreeValuesMatrix[which(abcDistances<=quantile(abcDistances, prob=abcTolerance)), ] #here's where we diy abc
+	abcResults$dist<-abcDistances[which(abcDistances<=quantile(abcDistances, prob=abcTolerance))]
 
+	print(paste("HERE", which(abcDistances<=quantile(abcDistances, prob=abcTolerance))))
+	particleDataFrame<-data.frame(cbind(rep(1, dim(abcResults$unadj.values)[1]), as.vector(which(abcDistances<=quantile(abcDistances, prob=abcTolerance))), seq(1:dim(abcResults$unadj.values)[1]), rep(0, dim(abcResults$unadj.values)[1]), abcResults$dist, rep(1, dim(abcResults$unadj.values)[1]), abcResults$unadj.values  ))
+	colnames(particleDataFrame)<-c("generation", "attempt", "id", "parentid", "distance", "weight",  paste("param", seq(dim(trueFreeValuesMatrix)[2])))
+
+	save(abcDistancesRaw, abcDistancesRawTotal, abcDistances, abcResults, particleDataFrame, file="BarbsDistances.Rdata")
+	input.data<-rbind(jobName, length(phy[[3]]), timeStep, StartSims, standardDevFactor, abcMethod, abcTolerance)
 
 	test<-vector("list")
 	#names(test)<-c("input.data", "PriorMatrix", "phy", "traits")
@@ -156,7 +163,7 @@ doRun_rej<-function(phy, traits, intrinsicFn, extrinsicFn, summaryFns=c(rawValue
 	test$traits<-traits
 	test$trueFreeValuesANDSummaryValues<-trueFreeValuesANDSummaryValues
   	test$SimTime<-simTime
-	test$unadj.values<-abcResults
+	test$particleDataFrame<-particleDataFrame
   
 	return(test)
 }
