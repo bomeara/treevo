ff83e067691e8e71f12a8235585b79c88cca32b1
Barb Banbury <bbanbury@users.noreply.github.com>
2012-06-27 21:13:21 +0000

    altered sumStatsNames function to accept phy instead of ntips

--- R/sumStatNames.R
+++ R/sumStatNames.R
@@ -1,3 +1,4 @@
-sumStatNames<-function(ntips=Ntip(phy)) {
+sumStatNames<-function(phy) {
+  ntips=Ntip(phy)
   return(c("BML", "BMbeta", "BMaic", "lambdaL", "lambdabeta", "lambdalambda", "lambdaaic", "deltaL", "deltabeta", "deltadelta", "deltaaic", "ouL", "oubeta", "oualpha", "ouaic", "whiteL", "whiteaic", "rawmean", "rawmax", "rawmin", "rawvar", "rawmedian", paste("tipVal", sequence(ntips), sep=""), paste("pic", sequence(ntips-1), sep=""), paste("ancState", sequence(ntips-1), sep=""), paste("ancCIrange", sequence(ntips-1), sep="")))
 }
