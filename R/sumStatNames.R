6e3a8a17b8d4dad27f157b904ad137499ccfc82b
Brian O'Meara <bomeara@users.noreply.github.com>
2012-06-27 19:28:16 +0000

    fixed error in the name indices

--- R/sumStatNames.R
+++ R/sumStatNames.R
@@ -1,3 +1,3 @@
 sumStatNames<-function(ntips=Ntip(phy)) {
-  return(c("BML", "BMbeta", "BMaic", "lambdaL", "lambdabeta", "lambdalambda", "lambdaaic", "deltaL", "deltabeta", "deltadelta", "deltaaic", "ouL", "oubeta", "oualpha", "ouaic", "whiteL", "whiteaic", "rawmean", "rawmax", "rawmin", "rawvar", "rawmedian", paste("tipVal", 1:ntips, sep=""), paste("pic", 1:ntips-1, sep=""), paste("ancState", 1:ntips-1, sep=""), paste("ancCIrange", 1:ntips-1, sep="")))
+  return(c("BML", "BMbeta", "BMaic", "lambdaL", "lambdabeta", "lambdalambda", "lambdaaic", "deltaL", "deltabeta", "deltadelta", "deltaaic", "ouL", "oubeta", "oualpha", "ouaic", "whiteL", "whiteaic", "rawmean", "rawmax", "rawmin", "rawvar", "rawmedian", paste("tipVal", sequence(ntips), sep=""), paste("pic", sequence(ntips-1), sep=""), paste("ancState", sequence(ntips-1), sep=""), paste("ancCIrange", sequence(ntips-1), sep="")))
 }
