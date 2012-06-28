989910ba45f749ecf4748bd695275d27b0cc09d6
Brian O'Meara <bomeara@users.noreply.github.com>
2012-06-28 20:31:09 +0000

    more intrinsic models

--- R/abcmodels.intrinsic.R
+++ R/abcmodels.intrinsic.R
@@ -161,3 +161,91 @@ autoregressiveIntrinsicTimeSlicesConstantSigma<-function(params,states, timefrom
 	newdisplacement<-rnorm(n=length(states),mean=(attractor-states)*attraction,sd=sd)
 	return(newdisplacement)
 }
+
+
+varyingBoundariesFixedSigmaIntrinsic<-function(params,states, timefrompresent) { #differing boundaries with time
+	#params=[sd, min1, max1, timethreshold1, min2, max2, timethreshold2, ...]
+	#time is time before present (i.e., 65 could be 65 MYA). The last time (present) threshold should be 0, one before that is the end of the previous epoch, etc.
+	numRegimes<-(length(params)-1)/3
+	#print(numRegimes)
+	timeSliceVector<-c(Inf)
+	for (regime in 1:numRegimes) {
+		timeSliceVector<-append(timeSliceVector,params[4+3*(regime-1)])
+	}
+	#timeSliceVector=c(Inf,params[which(c(1:length(params))%%4==0)])
+	#print(timeSliceVector)
+	sd<-params[1]
+	minBound<-params[2]
+	maxBound<-params[3]
+	for (regime in 1:numRegimes) {
+		#print(paste ("trying regime = ",regime))
+		if (timefrompresent<timeSliceVector[regime]) {
+			#print("timefrompresent>timeSliceVector[regime] == TRUE")
+			if (timefrompresent>=timeSliceVector[regime+1]) {
+				#print("timefrompresent>=timeSliceVector[regime+1] == TRUE")
+				#print(paste("chose regime ",regime))
+				#sd<-params[1+4*(regime-1)]
+				minBound<-params[2+3*(regime-1)]
+				maxBound<-params[3+3*(regime-1)]
+				#print(paste("sd = ",sd," attractor = ",attractor, " attraction = ", attraction))
+
+			}	
+		}	
+	}
+	#print(paste("sd = ",sd," attractor = ",attractor, " attraction = ", attraction))
+	newdisplacement<-rnorm(n=length(states),mean=0,sd=sd)
+		for (i in length(newdisplacement)) {
+		newstate<-newdisplacement[i]+states[i]
+		if (newstate<minBound) { #newstate less than min
+			newdisplacement[i]<-minBound-states[i] #so, rather than go below the minimum, this moves the new state to the minimum
+		}
+		if (newstate>maxBound) { #newstate greater than max
+			newdisplacement[i]<-maxBound-states[i] #so, rather than go above the maximum, this moves the new state to the maximum
+		}
+	}
+	return(newdisplacement)
+}
+
+varyingBoundariesVaryingSigmaIntrinsic<-function(params,states, timefrompresent) { #differing boundaries with time
+	#params=[sd1, min1, max1, timethreshold1, sd2, min2, max2, timethreshold2, ...]
+	#time is time before present (i.e., 65 could be 65 MYA). The last time (present) threshold should be 0, one before that is the end of the previous epoch, etc.
+	numRegimes<-(length(params))/3
+	#print(numRegimes)
+	timeSliceVector<-c(Inf)
+	for (regime in 1:numRegimes) {
+		timeSliceVector<-append(timeSliceVector,params[4+4*(regime-1)])
+	}
+	#timeSliceVector=c(Inf,params[which(c(1:length(params))%%4==0)])
+	#print(timeSliceVector)
+	sd<-params[1]
+	minBound<-params[2]
+	maxBound<-params[3]
+	for (regime in 1:numRegimes) {
+		#print(paste ("trying regime = ",regime))
+		if (timefrompresent<timeSliceVector[regime]) {
+			#print("timefrompresent>timeSliceVector[regime] == TRUE")
+			if (timefrompresent>=timeSliceVector[regime+1]) {
+				#print("timefrompresent>=timeSliceVector[regime+1] == TRUE")
+				#print(paste("chose regime ",regime))
+				#sd<-params[1+4*(regime-1)]
+				sd<-params[1+4*(regime-1)]
+				minBound<-params[2+4*(regime-1)]
+				maxBound<-params[3+4*(regime-1)]
+				#print(paste("sd = ",sd," attractor = ",attractor, " attraction = ", attraction))
+
+			}	
+		}	
+	}
+	#print(paste("sd = ",sd," attractor = ",attractor, " attraction = ", attraction))
+	newdisplacement<-rnorm(n=length(states),mean=0,sd=sd)
+		for (i in length(newdisplacement)) {
+		newstate<-newdisplacement[i]+states[i]
+		if (newstate<minBound) { #newstate less than min
+			newdisplacement[i]<-minBound-states[i] #so, rather than go below the minimum, this moves the new state to the minimum
+		}
+		if (newstate>maxBound) { #newstate greater than max
+			newdisplacement[i]<-maxBound-states[i] #so, rather than go above the maximum, this moves the new state to the maximum
+		}
+	}
+	return(newdisplacement)
+}
