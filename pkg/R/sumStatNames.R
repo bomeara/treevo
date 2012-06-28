dd71d90aa4df88bbc6443560b83702a850a138d8
bbanbury <bbanbury@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-06-28 16:36:25 +0000

    added boxcoxPlsRejection function

--- pkg/R/sumStatNames.R
+++ pkg/R/sumStatNames.R
@@ -1,4 +1,4 @@
 sumStatNames<-function(phy) {
   ntips=Ntip(phy)
-  return(c("BML", "BMbeta", "BMaic", "lambdaL", "lambdabeta", "lambdalambda", "lambdaaic", "deltaL", "deltabeta", "deltadelta", "deltaaic", "ouL", "oubeta", "oualpha", "ouaic", "whiteL", "whiteaic", "rawmean", "rawmax", "rawmin", "rawvar", "rawmedian", paste("tipVal", sequence(ntips), sep=""), paste("pic", sequence(ntips-1), sep=""), paste("ancState", sequence(ntips-1), sep=""), paste("ancCIrange", sequence(ntips-1), sep="")))
+  return(c("BML", "BMbeta", "BMaic", "lambdaL", "lambdabeta", "lambdalambda", "lambdaaic", "deltaL", "deltabeta", "deltadelta", "deltaaic", "ouL", "oubeta", "oualpha", "ouaic", "whiteL", "whiteaic", "rawmean", "rawmax", "rawmin", "rawvar", "rawmedian", phy$tip.label, paste("pic", sequence(ntips-1), sep=""), paste("ancState", sequence(ntips-1), sep=""), paste("ancCIrange", sequence(ntips-1), sep="")))
 }
