f6e7486dff0b68ab15213009160f7c7030500825
bcomeara <bcomeara@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-06-29 18:17:03 +0000

    making sure all is committed

--- pkg/R/boxcoxPlsRejection2.R
+++ pkg/R/boxcoxPlsRejection2.R
@@ -1,11 +1,17 @@
-boxcoxPlsRejection2<-function(summaryValuesMatrix, trueFreeValuesMatrix, phy, traits, vipthresh, abcMethod, abcTolerance){
+boxcoxPlsRejection2<-function(summaryValuesMatrix, trueFreeValuesMatrix, phy, traits, vipthresh, abcMethod, abcTolerance, verbose=TRUE){
 	#boxcox transform summary values
+	if (verbose) {
+		print("Starting boxcox transformation")
+	}
 	boxcoxEstimates<-boxcoxEstimation(summaryValuesMatrix)
 	boxcoxAddition<-boxcoxEstimates$boxcoxAddition
 	boxcoxLambda<-boxcoxEstimates$boxcoxLambda
 	boxcoxSummaryValuesMatrix<-boxcoxEstimates$boxcoxSummaryValuesMatrix
 	boxcoxOriginalSummaryStats<-boxcoxTransformation(summaryStatsLong(phy=phy, data=traits), boxcoxAddition, boxcoxLambda)
 
+	if (verbose) {
+		print("Starting to identify important summary stats")
+	}
 	library("mixOmics")
 	getVipSingleColumn<-function(trueFreeValuesColumn, boxcoxSummaryValuesMatrix) {
 		return(vip(pls(X=boxcoxSummaryValuesMatrix, Y=trueFreeValuesColumn, ncomp=1)))
@@ -19,19 +25,29 @@ boxcoxPlsRejection2<-function(summaryValuesMatrix, trueFreeValuesMatrix, phy, tr
 	rownames(whichVip)<-sumStatNames(phy)
 	
 	#save(trueFreeValuesMatrix, whichVip, boxcoxOriginalSummaryStats, boxcoxSummaryValuesMatrix, file="BarbsDistObjects.Rdata")
-	#calculatedDist<-abcDistance(trueFreeValuesMatrix, whichVip, boxcoxOriginalSummaryStats, boxcoxSummaryValuesMatrix) #THIS DOESN'T USE DIST IT STILL USES ABC!!!
-	abcDistancesRaw<-matrix(nrow=dim(trueFreeValuesMatrix)[1], ncol=dim(trueFreeValuesMatrix)[2])  #rep(0,dim(trueFreeValuesMatrix)[1]) #will hold the distances for each particle
-	abcDistancesRawTotal<-vector(length=dim(trueFreeValuesMatrix)[1])
+	if (verbose) {
+		print("Calculating distances")
+	}
+
+	calculatedDist<-abcDistance(trueFreeValuesMatrix, whichVip, boxcoxOriginalSummaryStats, boxcoxSummaryValuesMatrix) 
+	abcDistances<-calculatedDist$abcDistances
+	
+	#abcDistancesRaw<-matrix(nrow=dim(trueFreeValuesMatrix)[1], ncol=dim(trueFreeValuesMatrix)[2])  #rep(0,dim(trueFreeValuesMatrix)[1]) #will hold the distances for each particle
+	#abcDistancesRawTotal<-vector(length=dim(trueFreeValuesMatrix)[1])
 	#now we go true parameter by true parameter, using only the summary values with enough importance for each
 	#we get a distance for each particle from the observed particle for each true param
 	#then get a euclidean distance for all of these
 	#it's like getting delta X, then delta Y, and figuring out distance from the origin using
 	#sqrt((X-0)^2 + (Y-0)^2)
-	for (freeParamIndex in sequence(dim(trueFreeValuesMatrix)[2])) {
-		abcDistancesRaw[,freeParamIndex]<-abc(target=boxcoxOriginalSummaryStats[whichVip[,freeParamIndex]], param=trueFreeValuesMatrix[,freeParamIndex], sumstat= boxcoxSummaryValuesMatrix[,whichVip[,freeParamIndex]], tol=1, method=abcMethod)$dist^2 #because we're doing euclidean distance, from observed, which has value 0, 0, 0, etc.
+	#for (freeParamIndex in sequence(dim(trueFreeValuesMatrix)[2])) {
+	#	abcDistancesRaw[,freeParamIndex]<-abc(target=boxcoxOriginalSummaryStats[whichVip[,freeParamIndex]], param=trueFreeValuesMatrix[,freeParamIndex], sumstat= boxcoxSummaryValuesMatrix[,whichVip[,freeParamIndex]], tol=1, method=abcMethod)$dist^2 #because we're doing euclidean distance, from observed, which has value 0, 0, 0, etc.
+	#}
+	#abcDistancesRawTotal<-apply(abcDistancesRaw, 1, sum)
+	#abcDistances<-sqrt(abcDistancesRawTotal) #Euclid rules.
+	if (verbose) {
+		print("Getting accepted particles")
 	}
-	abcDistancesRawTotal<-apply(abcDistancesRaw, 1, sum)
-	abcDistances<-sqrt(abcDistancesRawTotal) #Euclid rules.
+
 
 	acceptedParticles<-trueFreeValuesMatrix[which(abcDistances<=quantile(abcDistances, prob=abcTolerance)), ] #here's where we diy abc
 	acceptedDistances<-abcDistances[which(abcDistances<=quantile(abcDistances, prob=abcTolerance))]
