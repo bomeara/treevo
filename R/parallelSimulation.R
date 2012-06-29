b55292525a4271538c978106b4a491065f1f63af
Brian O'Meara <bomeara@users.noreply.github.com>
2012-06-29 18:12:00 +0000

    now with checkpointing

--- R/parallelSimulation.R
+++ R/parallelSimulation.R
@@ -1,5 +1,5 @@
 
-parallelSimulation<-function(nrepSim, coreLimit, splits, phy, startingPriorsValues, intrinsicPriorsValues, extrinsicPriorsValues, startingPriorsFns, intrinsicPriorsFns, extrinsicPriorsFns, freevector, timeStep, intrinsicFn, extrinsicFn, multicore) {
+parallelSimulation<-function(nrepSim, coreLimit, splits, phy, startingPriorsValues, intrinsicPriorsValues, extrinsicPriorsValues, startingPriorsFns, intrinsicPriorsFns, extrinsicPriorsFns, freevector, timeStep, intrinsicFn, extrinsicFn, multicore,checkpointFile=NULL,checkpointFreq=24) {
 	library(doMC, quietly=T)
 	library(foreach, quietly=T)
 	cores=1
@@ -14,7 +14,30 @@ parallelSimulation<-function(nrepSim, coreLimit, splits, phy, startingPriorsValu
 		}
 	}
 	cat(paste("Using", cores, "core(s) for simulations \n\n"))
+	if (nrepSim %%cores != 0) {
+		warning("The simulation is most efficient if the number of nrepSim is a multiple of the number of cores")
+	}
+
 	cat("Doing simulations: ")
-	
-	trueFreeValuesANDSummaryValues<-foreach(1:nrepSim, .combine=rbind) %dopar% simulateData(splits, phy, startingPriorsValues, intrinsicPriorsValues, extrinsicPriorsValues, startingPriorsFns, intrinsicPriorsFns, extrinsicPriorsFns, freevector, timeStep, intrinsicFn, extrinsicFn)
+	if (is.null(checkpointFile)) {
+		trueFreeValuesANDSummaryValues<-foreach(1:nrepSim, .combine=rbind) %dopar% simulateData(splits, phy, startingPriorsValues, intrinsicPriorsValues, extrinsicPriorsValues, startingPriorsFns, intrinsicPriorsFns, extrinsicPriorsFns, freevector, timeStep, intrinsicFn, extrinsicFn)
+	}
+	else {
+		trueFreeValuesANDSummaryValues<-c()
+		checkpointFreqAdjusted<-max(cores*round(checkpointFreq/cores),1)
+		numberSimsInCheckpointRuns<-checkpointFreqAdjusted * floor(nrepSim/checkpointFreqAdjusted)
+		numberLoops<-floor(numberSimsInCheckpointRuns/checkpointFreqAdjusted)
+		numberSimsPerLoop<-numberSimsInCheckpointRuns/numberLoops
+		numberSimsAfterLastCheckpoint<-nrepSim - numberSimsInCheckpointRuns
+		if (checkpointFreqAdjusted != checkpointFreq ) {
+			warning(paste("Checkpoint frequency adjusted from",checkpointFreq,"to",checkpointFreqAdjusted,"to reduce the wasted time on unused cores"))
+		}
+		for (rep in sequence(numberLoops)) {
+			trueFreeValuesANDSummaryValues<-rbind(trueFreeValuesANDSummaryValues, foreach(1:numberSimsPerLoop, .combine=rbind) %dopar% simulateData(splits, phy, startingPriorsValues, intrinsicPriorsValues, extrinsicPriorsValues, startingPriorsFns, intrinsicPriorsFns, extrinsicPriorsFns, freevector, timeStep, intrinsicFn, extrinsicFn))
+			save(trueFreeValuesANDSummaryValues,file=checkpointFile)
+			print(paste("Just finished",dim(trueFreeValuesANDSummaryValues)[1],"of",nrepSim,"simulations; progress so far saved in",checkpointFile))
+		}
+		trueFreeValuesANDSummaryValues<-rbind(trueFreeValuesANDSummaryValues, foreach(1:numberSimsAfterLastCheckpoint, .combine=rbind) %dopar% simulateData(splits, phy, startingPriorsValues, intrinsicPriorsValues, extrinsicPriorsValues, startingPriorsFns, intrinsicPriorsFns, extrinsicPriorsFns, freevector, timeStep, intrinsicFn, extrinsicFn))
+	}
+	return(trueFreeValuesANDSummaryValues) 
 }
