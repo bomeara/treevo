69b42609e2aaaddd898193f4f8151d4a5380782d
Barb Banbury <bbanbury@users.noreply.github.com>
2012-07-06 23:52:50 +0000

    removed plsEstimation function and added more Rd files

--- R/boxcoxEstimation.R
+++ R/boxcoxEstimation.R
@@ -4,16 +4,14 @@ boxcoxEstimation<-function (summaryValuesMatrix) {
   boxcoxAddition <- rep(NA, dim(summaryValuesMatrix)[2])
   for (summaryValueIndex in 1:dim(summaryValuesMatrix)[2]) {
     boxcoxAddition[summaryValueIndex] <- 0
-    lowValue <- min(summaryValuesMatrix[, summaryValueIndex]) - 
-      4 * sd(summaryValuesMatrix[, summaryValueIndex])
+    lowValue <- min(summaryValuesMatrix[, summaryValueIndex]) - 4 * sd(summaryValuesMatrix[, summaryValueIndex])
     if (lowValue <= 0) {
       boxcoxAddition[summaryValueIndex] <- 4 * abs(lowValue)
     }
     summary <- summaryValuesMatrix[, summaryValueIndex] + boxcoxAddition[summaryValueIndex]
     boxcoxLambda[summaryValueIndex] <- 1
     if (sd(summaryValuesMatrix[, summaryValueIndex]) > 0) {
-      newLambda <- as.numeric(try(powerTransform(summary, 
-                                                 method = "Nelder-Mead")$lambda))
+      newLambda <- as.numeric(try(powerTransform(summary, method = "Nelder-Mead")$lambda))
       if (!is.na(newLambda)) {
         boxcoxLambda[summaryValueIndex] <- newLambda
       }
