c1ed309e40e4684003c554b538d1066f8523c823
Barb Banbury <bbanbury@users.noreply.github.com>
2012-08-09 18:43:45 +0000

    took out print statement

--- R/PLSTransform.R
+++ R/PLSTransform.R
@@ -2,6 +2,5 @@ PLSTransform<-function(summaryValuesMatrix, pls.model) {
   if (class(summaryValuesMatrix)!="matrix") {
     summaryValuesMatrix<-matrix(summaryValuesMatrix,ncol=max(c(1,length(summaryValuesMatrix)),na.rm=TRUE)) 
   }
-  print(paste("dim of summaryValuesMatrix is ",dim(summaryValuesMatrix)))
   predict(pls.model,summaryValuesMatrix,type="scores")
 }
