5c31e2700973af8de194ce63615b601ac09359dd
bbanbury <bbanbury@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-08-09 16:01:10 +0000

    working on pls--not done yet

--- pkg/R/boxcoxPlsRejection.R
+++ pkg/R/boxcoxPlsRejection.R
@@ -1,49 +1,20 @@
 boxcoxPlsRejection<-function(summaryValuesMatrix, trueFreeValuesMatrix, phy, traits, vipthresh, abcTolerance, verbose=TRUE){
-	#boxcox transform summary values
-	if (verbose) {
-		print("Starting boxcox transformation")
-	}
-	boxcoxEstimates<-boxcoxEstimation(summaryValuesMatrix)
-	boxcoxAddition<-boxcoxEstimates$boxcoxAddition
-	boxcoxLambda<-boxcoxEstimates$boxcoxLambda
-	boxcoxSummaryValuesMatrix<-boxcoxEstimates$boxcoxSummaryValuesMatrix
-	boxcoxOriginalSummaryStats<-boxcoxTransformation(summaryStatsLong(phy=phy, data=traits), boxcoxAddition, boxcoxLambda)
+	#uses library(pls)
+	#first, find number of components using cross validation
+	pls.model <- plsr(trueFreeValuesMatrix~summaryValuesMatrix,validation="CV",scale=TRUE) #scaling is important
+	explained.variance <-cumsum(sort(attr(scores(pls.model),"explvar"),decreasing=TRUE))
+	ncomp.final<-min(c(as.numeric(which(explained.variance>=95)[1]),length(explained.variance)),na.rm=TRUE) #min is to deal with case of never explaining >95%
+	pls.model.final <- plsr(trueFreeValuesMatrix~summaryValuesMatrix,ncomp=ncomp.final, validation="none",scale=TRUE) #now rerun with the ideal number of components
+	original.summary.stats<-summaryStatsLong(phy=phy, data=traits)
+	original.summary.stats.transformed<-predict(pls.model.final,matrix(original.summary.stats,nrow=1),type="scores")
+	simulated.summary.stats.transformed<-predict(pls.model.final,summaryValuesMatrix,type="scores")
+ 	
+	
+	calculatedDist<-abcDistance(trueFreeValuesMatrix,matrix(rep(TRUE,ncomp.final),nrow=1), original.summary.stats.transformed, simulated.summary.stats.transformed)$abcDistancesRaw[,1] #later, fix abcDistance 
 
-	if (verbose) {
-		print("Starting to identify important summary stats")
-	}
-	library("mixOmics")
-	getVipSingleColumn<-function(trueFreeValuesColumn, boxcoxSummaryValuesMatrix) {
-		return(vip(pls(X=boxcoxSummaryValuesMatrix, Y=trueFreeValuesColumn, ncomp=1)))
-	}
-	
-	#note this has vip for summary val 5, for true param 1, in row 5, column 1, and so forth
-	allVip<-apply(trueFreeValuesMatrix, 2, getVipSingleColumn, boxcoxSummaryValuesMatrix)
-	
-	#this will have which summary vals have importance above the threshold
-	whichVip<-(allVip>vipthresh)
-	rownames(whichVip)<-sumStatNames(phy)
-	
-	save(trueFreeValuesMatrix, whichVip, boxcoxOriginalSummaryStats, boxcoxSummaryValuesMatrix, file="BarbsDistObjects.Rdata")
-	if (verbose) {
-		print("Calculating distances")
-	}
+#open question: does the above procedure work best or is it better to do PLS for each parameter separately?
 
-	calculatedDist<-abcDistance(trueFreeValuesMatrix, whichVip, boxcoxOriginalSummaryStats, boxcoxSummaryValuesMatrix) 
-	abcDistances<-calculatedDist$abcDistances
-	
-	#abcDistancesRaw<-matrix(nrow=dim(trueFreeValuesMatrix)[1], ncol=dim(trueFreeValuesMatrix)[2])  #rep(0,dim(trueFreeValuesMatrix)[1]) #will hold the distances for each particle
-	#abcDistancesRawTotal<-vector(length=dim(trueFreeValuesMatrix)[1])
-	#now we go true parameter by true parameter, using only the summary values with enough importance for each
-	#we get a distance for each particle from the observed particle for each true param
-	#then get a euclidean distance for all of these
-	#it's like getting delta X, then delta Y, and figuring out distance from the origin using
-	#sqrt((X-0)^2 + (Y-0)^2)
-	#for (freeParamIndex in sequence(dim(trueFreeValuesMatrix)[2])) {
-	#	abcDistancesRaw[,freeParamIndex]<-abc(target=boxcoxOriginalSummaryStats[whichVip[,freeParamIndex]], param=trueFreeValuesMatrix[,freeParamIndex], sumstat= boxcoxSummaryValuesMatrix[,whichVip[,freeParamIndex]], tol=1, method=abcMethod)$dist^2 #because we're doing euclidean distance, from observed, which has value 0, 0, 0, etc.
-	#}
-	#abcDistancesRawTotal<-apply(abcDistancesRaw, 1, sum)
-	#abcDistances<-sqrt(abcDistancesRawTotal) #Euclid rules.
+
 	if (verbose) {
 		print("Getting accepted particles")
 	}
@@ -55,5 +26,5 @@ boxcoxPlsRejection<-function(summaryValuesMatrix, trueFreeValuesMatrix, phy, tra
 	particleDataFrame<-data.frame(cbind(rep(1, dim(acceptedParticles)[1]), as.vector(which(abcDistances<=quantile(abcDistances, prob=abcTolerance))), seq(1:dim(acceptedParticles)[1]), rep(0, dim(acceptedParticles)[1]), acceptedDistances, rep(1, dim(acceptedParticles)[1]), acceptedParticles))
 	colnames(particleDataFrame)<-c("generation", "attempt", "id", "parentid", "distance", "weight",  paste("param", seq(dim(trueFreeValuesMatrix)[2])))
 
-	return(list(particleDataFrame=particleDataFrame, abcDistancesRaw=calculatedDist$abcDistancesRaw, whichVip=whichVip, boxcoxSummaryValuesMatrix=boxcoxSummaryValuesMatrix, boxcoxOriginalSummaryStats=boxcoxOriginalSummaryStats))
+	return(list(particleDataFrame=particleDataFrame, calculatedDist=calculatedDist boxcoxSummaryValuesMatrix=boxcoxSummaryValuesMatrix, boxcoxOriginalSummaryStats=boxcoxOriginalSummaryStats))
 }
