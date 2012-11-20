3bf77c79f4e16cdff30497af0df06ab3af385637
bbanbury <bbanbury@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-11-20 18:18:00 +0000

    added GetExpPushPriors functions to the end of abcmodels.extrinsic.R

--- pkg/R/abcmodels.extrinsic.R
+++ pkg/R/abcmodels.extrinsic.R
@@ -58,3 +58,42 @@ everyoneDisplacementExtrinsic<-function(params,selfstates,otherstates, timefromp
 	return(newdisplacement)
 }
 
+
+
+#Extra functions for calculating Exponential Decay Push priors
+GetExpPushPriors<-function(numSteps, data) {
+  #returns a matrix with exponential rates for the three Exponential push priors
+  sd<-GetBMRatePrior(phy, traits, timeStep)  #new TreEvo function
+  data.sort<-sort(data[,1])
+  data.min.diff<-min(abs(data.sort[1:(length(data.sort)-1)]-data.sort[2:(length(data.sort))]))
+  data.max.diff<-abs(max(data.sort)-min(data.sort))
+  half.distance.prior<-EstimateRate(data.min.diff)
+  max.push.prior<-EstimateMaxForce(data.max.diff, halfDistance=qexp(0.5, half.distance.prior), numSteps)
+  return(matrix(c(sd, sd, half.distance.prior, half.distance.prior, max.push.prior, max.push.prior), nrow=2))  #each one is doubled so they will exp rate param will fit into intrinsicPriorValues matrix in doRun_rej
+}
+qexpOptimality<-function(x, q, diff) {
+  return(abs(qexp(q,x)-diff))
+}
+EstimateRate<-function(diff, quantile=0.95) {
+  result<-optimize(f=qexpOptimality, interval = c(0, 100000), q=quantile, diff=diff)
+  return(result$minimum)
+}
+OneStepPush<-function(maxForce, halfDistance, state) {
+  rate<-log(2, base=exp(1))/ halfDistance
+  push<-maxForce*2*exp(-1*rate*abs(state))
+  return(push)
+}
+MultiStepPush<-function(maxForce, halfDistance, numSteps) {
+  state<-0
+  for (i in sequence(numSteps)) {
+    state<-OneStepPush(maxForce, halfDistance, state)+state
+  }
+  return(state)
+}
+ForceOptimality<-function(x, halfDistance, numSteps, diff) {
+  return(abs(diff - MultiStepPush(x, halfDistance, numSteps)))
+}
+EstimateMaxForce<-function(diff, halfDistance, numSteps) {
+  result<-optimize(f=ForceOptimality, interval = c(0, 100000), halfDistance=halfDistance, numSteps = numSteps, diff=diff)
+  return(result$minimum)
+}
