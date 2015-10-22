50a0f3116d93959a855ddea5fb269289ee30ebde
Brian OMeara <omeara.brian@gmail.com>
2015-10-22 15:54:03 -0400

    adding testthat checking

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(TreEvo)
+
+test_check("TreEvo")
---
+++ tests/testthat/test_doRun_PRC.R
@@ -0,0 +1,28 @@
+test_that("doPRC runs correctly", {
+	data(simData)
+	results <- doRun_prc(
+	  phy = simPhy,
+	  traits = simChar,
+	  intrinsicFn=brownianIntrinsic,
+	  extrinsicFn=nullExtrinsic,
+	  startingPriorsFns="normal",
+	  startingPriorsValues=matrix(c(mean(simChar[,1]), sd(simChar[,1]))),
+	  intrinsicPriorsFns=c("exponential"),
+	  intrinsicPriorsValues=matrix(c(10, 10), nrow=2, byrow=FALSE),
+	  extrinsicPriorsFns=c("fixed"),
+	  extrinsicPriorsValues=matrix(c(0, 0), nrow=2, byrow=FALSE),
+	  TreeYears=1000,
+	  standardDevFactor=0.2,
+	  plot=FALSE,
+	  StartSims=300,
+	  epsilonProportion=0.7,
+	  epsilonMultiplier=0.7,
+	  nStepsPRC=5,
+	  numParticles=100,
+	  jobName="exampleRun",
+	  stopRule=FALSE,
+	  multicore=FALSE,
+	  coreLimit=1
+	)
+	expect_is(results, "list")	
+})
