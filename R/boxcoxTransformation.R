b22fc7b2b7bed75a3a42415f244898625c43fd6b
David Bapst <dwbapst@gmail.com>
2017-09-05 14:14:18 -0500

    Merged boxcox funs

--- R/boxcoxTransformation.R
+++ R/boxcoxTransformation.R
@@ -1,28 +1,38 @@
-#' Box-Cox Transformation
+#' Box-Cox Transformation for Simulations
 #' 
-#' This function Box-Cox transforms summary values from a single simulation
+#' \code{boxcoxTransformation} Box-Cox transforms summary values from a single simulation, while
+#' \code{boxcoxTransformationMatrix} Box-Cox transforms summary values from multiple simulations.
 #' 
 #' 
-#' @param summaryValues Vector of summary statistics
+
+#' @param summaryValuesMatrix Matrix of summary statistics from simulations
+
+#' @param summaryValuesVector Vector of summary statistics from a single simulation
 
 #' @param boxcoxAddition Vector of boxcox additions from boxcoxEstimation
 
 #' @param boxcoxLambda Vector of boxcox lambda values from boxcoxEstimation
 
-#' @return Returns a vector of Box-Cox transformed summary statistics from a
-#' single observation.
+#' @return \code{boxcoxTransformation} returns a vector of Box-Cox transformed summary statistics from a
+#' single observation. \code{boxcoxTransformationMatrix} returns a matrix of Box-Cox transformed summary statistics with the
+#' same dimensions as \code{summaryValuesMatrix}.
 
 #' @author Brian O'Meara and Barb Banbury
 
 # @references O'Meara and Banbury, unpublished; Bates et al. 2009
-# @keywords boxcoxTransformation Box-Cox
+# @keywords boxcoxEstimation Box-Cox
 
 #' @examples
-#' # this needs examples
+#' 
+#' data(simRun)
+#' 
+#' boxcoxTransformation(summaryValuesMatrix)
+#' 
+#' # boxcoxTransformationMatrix needs examples
 #'
 
-#' @name abcDistance
-#' @rdname abcDistance
+#' @name boxcoxTransformation
+#' @rdname boxcoxTransformation
 #' @export
 boxcoxTransformation<-function(summaryValues, boxcoxAddition, boxcoxLambda) { #yes, a row of summary values
 	for (summaryValueIndex in 1:length(summaryValues)) {
@@ -30,4 +40,30 @@ boxcoxTransformation<-function(summaryValues, boxcoxAddition, boxcoxLambda) { #y
             boxcoxAddition[summaryValueIndex])^boxcoxLambda[summaryValueIndex]
     }
     return(summaryValues)
+}
+
+#' @rdname boxcoxTransformation
+#' @export
+boxcoxTransformationMatrix<-function (summaryValuesMatrix) {
+  #library("car", quietly = T)
+  boxcoxLambda <- rep(NA, dim(summaryValuesMatrix)[2])
+  boxcoxAddition <- rep(NA, dim(summaryValuesMatrix)[2])
+  for (summaryValueIndex in 1:dim(summaryValuesMatrix)[2]) {
+    boxcoxAddition[summaryValueIndex] <- 0
+    lowValue <- min(summaryValuesMatrix[, summaryValueIndex]) - 4 * sd(summaryValuesMatrix[, summaryValueIndex])
+    if (lowValue <= 0) {
+      boxcoxAddition[summaryValueIndex] <- 4 * abs(lowValue)
+    }
+    summary <- summaryValuesMatrix[, summaryValueIndex] + boxcoxAddition[summaryValueIndex]
+    boxcoxLambda[summaryValueIndex] <- 1
+    if (sd(summaryValuesMatrix[, summaryValueIndex]) > 0) {
+      newLambda <- as.numeric(try(powerTransform(summary, method = "Nelder-Mead")$lambda))
+      if (!is.na(newLambda)) {
+        boxcoxLambda[summaryValueIndex] <- newLambda
+      }
+    }
+    summaryValuesMatrix[, summaryValueIndex] <- summary^boxcoxLambda[summaryValueIndex]
+  }
+  return(list(boxcoxAddition = boxcoxAddition, boxcoxLambda = boxcoxLambda, 
+              boxcoxSummaryValuesMatrix = summaryValuesMatrix))
 }
---
+++ R/simRun.R
@@ -0,0 +1,18 @@
+#' Simulated data
+#'
+#' Simulated 30-taxon coalescent tree and simulated character data from BM
+#' intrinsic model. Character data was generated using doSimulation.
+#' Also includes results from an example analysis.
+#'
+#'
+#' @name simRun
+
+#' @aliases simRun simPhy simChar results
+
+#' @docType data
+
+#' @format simPhy in phylo format and simChar in single column matrix and results
+
+# @keywords datasets
+NULL
+
