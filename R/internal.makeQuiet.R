23490f57aeb48a40cf5ea23b604f83118261ba16
David Bapst <dwbapst@gmail.com>
2017-10-13 11:23:59 -0500

    Bumped version number, made DWB maintainer

--- R/internal.makeQuiet.R
+++ R/internal.makeQuiet.R
@@ -1,29 +1,32 @@
 # internal function, to quiet another function's print/message/warning statements
 
-# see this for more:
+# @author David Bapst
+
+# see this for more details on why I chose the functions I went with:
 # https://stackoverflow.com/questions/8797314/suppress-messages-displayed-by-print-instead-of-message-or-warning-in-r
 
 makeQuiet<-function(funToEval){
 	noise<-invisible(suppressWarnings(suppressMessages(capture.output(
-		result<-eval(funToEval)))))
-	return(invisible(eval(funToEval)))
+		result<-eval(funToEval)
+		))))
+	return(invisible(result))
 	}
 
 
-# EXAMPLE 
+# @example 
 #
 # fun <- function(x){
 #   print("Thanks for using my function!!")
 #	message("How about that?")
-#   if(is(x, "numeric"))
-#     print("warning, x should be a character")
+#   print("warning, x should be a character")
 #	warning("I can be so noisy!")
 #   cat("Oh my swirls!")
 #   return(x+1)
 #	}
 #
-# makeQuiet(fun(13))
+# makeQuiet(fun(13)) # won't do anything, cause output is invisible()
 #
+# # these should work, though
 # ab<-makeQuiet(fun(19))
 # ab
 #
