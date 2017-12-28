47ffb9344c75b096bcc8c5c1de13a19398fd3cd3
David Bapst <dwbapst@gmail.com>
2017-12-28 15:45:05 -0800

    Trying to decrease test run time

--- tests/testthat/test_0_pre-existing_tests.R
+++ tests/testthat/test_0_pre-existing_tests.R
@@ -16,36 +16,6 @@ test_that("simulation ran", {
 )
 
 
-test_that("doPRC runs correctly", {
-	data(simRunExample)
-	set.seed(1)
-	results <- doRun_prc(
-	  phy = simPhy,
-	  traits = simChar,
-	  intrinsicFn=brownianIntrinsic,
-	  extrinsicFn=nullExtrinsic,
-	  startingPriorsFns="normal",
-	  startingPriorsValues=matrix(c(mean(simChar[,1]), sd(simChar[,1]))),
-	  intrinsicPriorsFns=c("exponential"),
-	  intrinsicPriorsValues=matrix(c(10, 10), nrow=2, byrow=FALSE),
-	  extrinsicPriorsFns=c("fixed"),
-	  extrinsicPriorsValues=matrix(c(0, 0), nrow=2, byrow=FALSE),
-	  generation.time=100000,
-	  standardDevFactor=0.2,
-	  plot=FALSE,
-	  StartSims=10,
-	  epsilonProportion=0.7,
-	  epsilonMultiplier=0.7,
-	  nStepsPRC=3,
-	  numParticles=20,
-	  jobName="exampleRun",
-	  stopRule=FALSE,
-	  multicore=FALSE,
-	  coreLimit=1
-	  )
-	expect_is(results, "list")
-	expect_false(any(is.na(pairwiseESS(results$particleDataFrame))))
-})
 
 
 test_that("plotting works", {
--- tests/testthat/test_doRun.R
+++ tests/testthat/test_doRun.R
@@ -1,26 +1,49 @@
-test_that("doRun works", {
+test_that("doRun_prc runs correctly", {
+	data(simRunExample)
+	set.seed(1)
+	results <- doRun_prc(
+	  phy = simPhy,
+	  traits = simChar,
+	  intrinsicFn=brownianIntrinsic,
+	  extrinsicFn=nullExtrinsic,
+	  startingPriorsFns="normal",
+	  startingPriorsValues=matrix(c(mean(simChar[,1]), sd(simChar[,1]))),
+	  intrinsicPriorsFns=c("exponential"),
+	  intrinsicPriorsValues=matrix(c(10, 10), nrow=2, byrow=FALSE),
+	  extrinsicPriorsFns=c("fixed"),
+	  extrinsicPriorsValues=matrix(c(0, 0), nrow=2, byrow=FALSE),
+	  generation.time=100000,
+	  standardDevFactor=0.2,
+	  plot=FALSE,
+	  StartSims=10,
+	  epsilonProportion=0.7,
+	  epsilonMultiplier=0.7,
+	  nStepsPRC=3,
+	  numParticles=20,
+	  jobName="exampleRun",
+	  stopRule=FALSE,
+	  multicore=FALSE,
+	  coreLimit=1
+	  )
+	expect_is(results, "list")
+	expect_false(any(is.na(pairwiseESS(results$particleDataFrame))))
+})
+
+
+
+test_that("doRun_rej works", {
   data(simRunExample)
-  resultsPRC <- doRun_prc(phy = simPhy, traits = simChar,
-    intrinsicFn = brownianIntrinsic, extrinsicFn = nullExtrinsic,
-    startingPriorsFns = "normal", startingPriorsValues = matrix(c(mean(simChar[,
-      1]), sd(simChar[, 1]))), intrinsicPriorsFns = c("exponential"),
-    intrinsicPriorsValues = matrix(c(10, 10), nrow = 2,
-      byrow = FALSE), extrinsicPriorsFns = c("fixed"),
-    extrinsicPriorsValues = matrix(c(0, 0), nrow = 2,
-      byrow = FALSE), generation.time = 1e+05, standardDevFactor = 0.2,
-    plot = FALSE, StartSims = 10, epsilonProportion = 0.7,
-    epsilonMultiplier = 0.7, nStepsPRC = 3, numParticles = 20,
-    jobName = "examplerun_prc", stopRule = FALSE, multicore = FALSE,
-    coreLimit = 1)
-  resultsPRC
-  resultsRej <- doRun_rej(phy = simPhy, traits = simChar,
-    intrinsicFn = brownianIntrinsic, extrinsicFn = nullExtrinsic,
-    startingPriorsFns = "normal", startingPriorsValues = matrix(c(mean(simChar[,
-      1]), sd(simChar[, 1]))), intrinsicPriorsFns = c("exponential"),
+	set.seed(1)
+   resultsRej <- doRun_rej(phy = simPhy, traits = simChar,
+    intrinsicFn = brownianIntrinsic, 
+	extrinsicFn = nullExtrinsic,
+    startingPriorsFns = "normal", 
+	startingPriorsValues = matrix(c(mean(simChar[,1]), sd(simChar[, 1]))), 
+	  intrinsicPriorsFns = c("exponential"),
     intrinsicPriorsValues = matrix(c(10, 10), nrow = 2,
       byrow = FALSE), extrinsicPriorsFns = c("fixed"),
     extrinsicPriorsValues = matrix(c(0, 0), nrow = 2,
       byrow = FALSE), StartSims = 10, jobName = "examplerun_rej",
     abcTolerance = 0.05, multicore = FALSE, coreLimit = 1)
-  resultsRej
 })
+
